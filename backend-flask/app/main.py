949cb2b76667775c11d47f16ae291b38abfd9051
riskiwah <riskic44@gmail.com>
2020-07-09 18:49:03 +0700

    bump update critical api-key; update npm package

--- backend-flask/app/main.py
+++ backend-flask/app/main.py
@@ -1,13 +1,15 @@
+import requests
+import os
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-import requests
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/cuaca', methods=['GET'])
 def cuaca():
-    API_KEY = '947eb74ab694945d3f9b228bd8250d5a'
+    API_KEY = os.getenv('KEY')
     CITY = 'Malang'
 
     r = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric".format(CITY, API_KEY)
